a4656d3889670098066459429b76e2042236ca87
Shamel Lakin <christlife4u@gmail.com>
2019-09-17 18:00:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
 
+    collection << 
+    yield(array[i])
+    i += 1
+  end
+  array
+end
 
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |student|
+  student.split(" ").first
+end
+
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |language|
+  language.upcase
+end
