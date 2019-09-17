acbc6ca40bff3290f0f5644887965b9ce1e9c66e
Shamel Lakin <christlife4u@gmail.com>
2019-09-17 17:47:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,12 +10,3 @@ def my_collect(array)
   array
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
