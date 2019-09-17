0abfb43235c96192e75af7f756468c1b1c571196
Shamel Lakin <christlife4u@gmail.com>
2019-09-17 17:44:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -15,4 +15,7 @@ my_collect(students) do |student|
   student.split(" ").first
 end
 
-
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
